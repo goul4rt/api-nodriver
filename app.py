28cf8f31911117fc77e8647cb0671498a6a7b600
Aroldo <aroldogooulart@gmail.com>
2025-02-27 13:22:54 -0400

    s

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def scrape():
     return jsonify({"url": url, "html": html_content})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3333)
+    app.run(debug=False, host='0.0.0.0', port=3333)
